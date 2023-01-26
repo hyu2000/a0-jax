25da7227e770c62c020e1052034fe3d68ba0fb61
Hua Yu <hyu2000@gmail.com>
2023-01-26 13:12:46 -0500

    see how fast 9x9 selfplay is

---
+++ train_go9.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+TF_CPP_MIN_LOG_LEVEL=2 \
+python3 train_agent.py \
+    --game-class="games.go_game.GoBoard9x9" \
+    --agent-class="policies.resnet_policy.ResnetPolicyValueNet128" \
+    --random-seed=42 \
+    --ckpt-filebase="/content/drive/MyDrive/dlgo/9x9/go_agent_9" \
+    --selfplay-batch-size=64 \
+    --training-batch-size=128 \
+    --learning-rate=1e-2 \
+    --lr-decay-steps=1000000 \
+    --num-simulations-per-move=128 \
+    --num-simulations-per-move-eval=128 \
+    --num-self-plays-per-iteration=512 \
+    --num-eval-games=32 \
+    --num-iterations=1
+
+#     --ckpt-filebase="./exp-go5C2/go_agent_5" \
