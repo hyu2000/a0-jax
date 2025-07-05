ac28d36511f3ad6ef618e35bd27d80bd66d6a228
Hua Yu <hyu2000@gmail.com>
2025-07-05 09:20:25 -0400

    colab shell doesn't like comments after \

--- train_go5.sh
+++ train_go5.sh
@@ -8,13 +8,16 @@ python3 train_agent.py \
     --random-seed=42 \
     --ckpt-filebase="/content/drive/MyDrive/dlgo/5x5/a0jax/exp-go5C2/go_agent_5" \
     --selfplay-batch-size=64 \
-    --training-batch-size=1024 \  # this goes with LR. 1024 is about 32 games
+    --training-batch-size=1024 \
     --learning-rate=1e-2 \
-    --lr-decay-steps=4096 \  # 60 gen * 256/32 * 8 (symmetries) = 480 * 8
+    --lr-decay-steps=4096 \
     --num-simulations-per-move=32 \
     --num-simulations-per-move-eval=32 \
-    --num-self-plays-per-iteration=256 \  # num games per generation
+    --num-self-plays-per-iteration=256 \
     --num-eval-games=32 \
     --num-iterations=180
 
 #     --ckpt-filebase="./exp-go5C2/go_agent_5" \
+#    --training-batch-size=1024 \  # this goes with LR. 1024 is about 32 games
+#    --lr-decay-steps=4096 \  # 60 gen * 256/32 * 8 (symmetries) = 480 * 8
+#    --num-self-plays-per-iteration=256 \  # num games per generation
